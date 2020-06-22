49413fb2497c3acb937374f36111acfd57ada86f
Hyunwoo Kim <hwoo1211@gmail.com>
2020-06-21 22:27:46 -0500

    Added comments to js file

--- getPoints.js
+++ getPoints.js
@@ -1,29 +1,32 @@
 /* HTML DOM Elements */
 
-var wSelection = document.querySelector('#world')
-var tSelection = document.querySelector('#town')
-var pts = document.getElementById("points")
+var wSelection = document.querySelector('#world')   // World selection dropdown box
+var tSelection = document.querySelector('#town')    // Town selection dropdown box
+var pts = document.getElementById("points")         // All map points
 
 /* Other global variables*/
 
-var links = []
-var baseWorldMapUrl = 'https://maplestory.io/api/KMST/1101/map/worldmap/'
-var baseMapUrl = 'https://maplestory.io/api/KMST/1101/map/'
-var url;
-var val
+var links = []  // Array of maps of links
+var baseWorldMapUrl = 'https://maplestory.io/api/KMST/1101/map/worldmap/' // WorldMap URL
+var baseMapUrl = 'https://maplestory.io/api/KMST/1101/map/'               // Map URL
+var url; // Temp url variable that will be used throughout the code
+var val // Temp variable 
 
 /* Event Listeners */
 
-wSelection.addEventListener("change", function () {
-    initializeDropdown();
-    mapChange(wSelection.value)
-    loadMap()
+// World Selection Dropdown Box Event
+wSelection.addEventListener("change", function () { // When this box experiences change
+    initializeDropdown();         // Delete extraneous towns in tSelect dropdown box
+    mapChange(wSelection.value)   // Change to appropriate map
+    loadMap()                     // Then get the points
 });
-tSelection.addEventListener("change", function () {
-    if(tSelection.value != 'default')
+
+// Town Seleciton Dropdown Box Event
+tSelection.addEventListener("change", function () { // When this box experiences change
+    if(tSelection.value != 'default') // If the selected option is not default
     {
-        mapChange(tSelection.value)
-        loadMap()
+        mapChange(tSelection.value)   // Then change to appropriate map
+        loadMap()                     // Then get the points
     }
 });
 
@@ -35,16 +38,15 @@ tSelection.addEventListener("change", function () {
 //var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function Get(url){
-    var request = new XMLHttpRequest(); // a new request
-    request.open("GET",url,false);
+    var request = new XMLHttpRequest(); // Instantiate new request
+    request.open("GET",url,false);      // Open URL
     request.send(null);
-    if (request.status != 404)
+    if (request.status != 404)          // If the status is good
     {
-        
-        return request.responseText;
+        return request.responseText;    // return the text of the request
     }
     else
-        return null;           
+        return null;                    // In case of 404, discard. 
 }
 
 function getTownName(mapNum) {
@@ -58,39 +60,45 @@ function getTownName(mapNum) {
         return null
 }
 
-function initializeDropdown() {
+// Initialization  of the tSelection dropdown box. 
+function initializeDropdown() {  
     while (tSelection.options.length != 1)
     {
-        tSelection.options.remove(tSelection.options.length - 1)
+        tSelection.options.remove(tSelection.options.length - 1) // Remove every element except 'default'
     }
-    tSelection.value = 'default';
+    tSelection.value = 'default'; // Then select the default option
 }
 
 /* This function takes care of loading the points on the map*/
 
 function loadMap() {
-    links = []
-    
+    links = []     // Empty the array
 
-    while (pts.firstChild)
+    while (pts.firstChild) // Remove all points from the screen
     {
         pts.removeChild(pts.firstChild)
     }
 
-    let mapinfo_json = JSON.parse(Get(url));
-    let origin = mapinfo_json["baseImage"][0]["origin"]["value"]
+    let mapinfo_json = JSON.parse(Get(url)); // Fetch JSON file 
+    let origin = mapinfo_json["baseImage"][0]["origin"]["value"] // Find the origin of the map file
     for(let i = 0; i < mapinfo_json["maps"].length; i++)
     {
-        let type = mapinfo_json["maps"][i]["type"]
+        let type = mapinfo_json["maps"][i]["type"] // This gives the type of point (town, starforce, etc)
+
+        // This is the coordinate for the point
         let point = [origin["x"]+mapinfo_json["maps"][i]["spot"]["value"]["x"], origin["y"]+mapinfo_json["maps"][i]["spot"]["value"]["y"]]
-        let imgstr = "<img src='images/points/mapImage_" + type + ".png'" + 
-                /*"id='" + getTownName(mapinfo_json["maps"][i]["mapNumbers"][0]) + "'" +*/
-                "class='pts' style='position: absolute; left: " + 
+        
+        // String to add to the HTML
+        let imgstr = "<img src='images/points/mapImage_" + type + ".png'" + // this is image source
+                /*"id='" + getTownName(mapinfo_json["maps"][i]["mapNumbers"][0]) + "'" +*/  // Id of the image
+                "class='pts' style='position: absolute; left: " + // This places the points on the right place on the map
                 (point[0]-returnSize(type)).toString() + "px ; top: " + (point[1]-returnSize(type)).toString() + "px;'>"
 
+        // Add to HTML
         document.getElementById('points').innerHTML += imgstr
     }
 
+    // Add to the tSelect Dropdown box
     for (let j = 0; j < mapinfo_json["links"].length; j++)
     {
         var opt = document.createElement("option")
@@ -100,29 +108,15 @@ function loadMap() {
 
         tSelection.options.add(opt);
     }
-
-    //let maps = []
-    
-    /*for (let k = 0; k < mapinfo_json["maps"].length; k++) 
-    {
-        let tempMap = baseMapUrl + mapinfo_json["maps"][k]["mapNumbers"][0].toString();
-        let tempMapinfo_json = JSON.parse(Get(tempMap))
-        maps.push(tempMapinfo_json["name"])
-
-
-    }*/
 }
 
+
+// Change the map based on the option given
 function mapChange(val) {
     url = baseWorldMapUrl + val;
     document.querySelector('#mapvis').src = 'images/wMaps/' + val + '.png' 
 }
 
-/*function townChange(val) {
-    switch(val) {
-
-    }
-}*/
 
 
 /* This function returns the radius of each point necessary for
@@ -156,6 +150,9 @@ function returnSize (num) {
     }
 }
 
-mapChange(wSelection.value) // Initialize the map
+
+
+// Functions to be run on initial load
+mapChange(wSelection.value)
 loadMap(); 
 
